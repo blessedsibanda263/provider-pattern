4ad7b2683b0c8a0b8d70fadfb0de6eb4a4983074
Blessed <zimelixir@gmail.com>
2024-10-24 04:23:10 +0200

    Dark mode with context

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createContext, memo, useContext, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const DarkModeContext = createContext({});
 
+function Button({ children, ...rest }) {
+  const { isDarkMode } = useContext(DarkModeContext);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <button
+      style={{
+        backgroundColor: isDarkMode ? "#333" : "#ccc",
+        border: "1px solid",
+        color: "inherit",
+      }}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
 }
 
-export default App
+function ToggleButton() {
+  const { toggleDarkMode } = useContext(DarkModeContext);
+  return <Button onClick={toggleDarkMode}>Toggle mode</Button>;
+}
+
+const Header = memo(function Header() {
+  return (
+    <header
+      style={{
+        padding: "10px 5px",
+        borderBottom: "1px solid",
+        marginBottom: "10px",
+        display: "flex",
+        gap: "5px",
+        justifyContent: "flex-end",
+      }}
+    >
+      <Button>Products</Button>
+      <Button>Services</Button>
+      <Button>Pricing</Button>
+      <ToggleButton />
+    </header>
+  );
+});
+
+const Main = memo(function Main() {
+  const { isDarkMode } = useContext(DarkModeContext);
+  return (
+    <main
+      style={{
+        color: isDarkMode ? "white" : "black",
+        backgroundColor: isDarkMode ? "black" : "white",
+        margin: "-8px",
+        minHeight: "100vh",
+        boxSizing: "border-box",
+      }}
+    >
+      <Header />
+      <h1>Welcome to our business site!</h1>
+    </main>
+  );
+});
+
+function App() {
+  const [isDarkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = () => setDarkMode((v) => !v);
+  const contextValue = { isDarkMode, toggleDarkMode };
+  return (
+    <DarkModeContext.Provider value={contextValue}>
+      <Main />
+    </DarkModeContext.Provider>
+  );
+}
+
+export default App;
